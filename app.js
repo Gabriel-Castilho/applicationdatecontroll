315ddc59173f29874639fafdebb659fd0a8c3fe1
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-03-27 14:38:27 -0300

    create home page

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express();
+
+app.set('view engine','ejs')
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //accepting json
 
 
 app.get("/",(req,res)=>{
-    res.json({message:"hello"})
+    res.render("index")
 })
 
 
