a153e3009de353b71ddce50ec08cf9d688e2bb3c
Gabriel Alexandre <gabriel.castilho@outlook.com.br>
2021-03-27 15:10:29 -0300

    .

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const bodyParser = require("body-parser")
 const axios = require("axios")
 const app = express();
 const api = require("./services/api")
+const cors=require("cors")
 
 app.set('view engine','ejs')
 
-
+app.use(cors())
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //accepting json
